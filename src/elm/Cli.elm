b82d9c039fc2125d2b96a8e50ca3be2261695995
David.Vader <david.vader@target.com>
2020-02-02 23:58:42 -0600

    init

---
+++ src/elm/Cli.elm
@@ -0,0 +1,19 @@
+{--
+Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+Use of this source code is governed by the LICENSE file in this repository.
+--}
+
+
+module Cli exposing (view)
+
+import Html exposing (Html, text)
+
+
+view : Html msg
+view =
+    text "cli help"
+
+
+viewHelp : Html msg
+viewHelp =
+    text "cli help"
